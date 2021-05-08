6fd8552695b2e7102379176263b2b41f88c1b9e6
Steve Meyer <sfmeyer@gmail.com>
2021-05-08 15:13:08 -0700

    midpoint

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,19 @@ var tbody = d3.select("tbody");
 console.log(tableData);
 
 // Create array with the column names 
-var columns = ["datetime","city","state","country","shape","durationMinutes","comments"]
+var columns = ["datetime","city","state","country","shape","durationMinutes","comments"];
 
+// Loop through data, append each row to html table
+function loadData(){
+    tableData.forEach(x => {
+        var row = tbody.append("tr")
+        columns.forEach(column => {
+            if(column =="city" || column =="state" || column == "country"){
+                row.append("td").text(x[column])
+            }
+              else row.append("td").text(x[column])    
+        })
+    })
+}
 
+loadData()

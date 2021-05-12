3f2e1cab4174b11b00ab1a816a4cb4e88c334e35
Steve Meyer <sfmeyer@gmail.com>
2021-05-11 18:24:27 -0700

    near done

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,42 @@
 // from data.js
 var tableData = data;
 
-// Reference the tbody in the html
+// reference the tbody 
 var tbody = d3.select("tbody");
 
-// Console.log the data variable
+// console.log the data variable
 console.log(tableData);
 
 // Create array with the column names 
 var columns = ["datetime","city","state","country","shape","durationMinutes","comments"];
 
-// Loop through data, append each row to html table
+// Loop through data file, append each row to table
 function loadData(){
     tableData.forEach(x => {
         var row = tbody.append("tr")
         columns.forEach(column => {
-            if(column =="city" || column =="state" || column == "country"){
-                row.append("td").text(x[column])
-            }
-              else row.append("td").text(x[column])    
-        })
-    })
-}
+        row.append("td").text(x[column])    
+        });
+    });
+};
 
-loadData()
+// execute the function
+loadData();
+
+// -------------------------------
+// setup of variables for d3 elements
+// make variable references to the d3 objects according to id  
+var inDate = d3.select("#datetime");
+var inCity = d3.select("#city");
+var inState = d3.select("#state");
+var inCountry = d3.select("#country");
+var inShape = d3.select("#shape");
+
+// variable for filter button with id = 'filter-button'
+var filterButton = d3.select("#filter-button");
+
+// variable for reset button with id = 'reset-button'
+var resetButton = d3.select("#reset-button");
+
+
+
